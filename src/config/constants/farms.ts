c093f13f57d4fe1908ec4273aa1135d7b4ba61e3
brunomacae <83554988+brunomacae@users.noreply.github.com>
2021-07-18 17:44:13 -0300

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -10,7 +10,7 @@ const farms: FarmConfig[] = [
     lpSymbol: 'CAKE',
     lpAddresses: {
       97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
-      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      56: '0x145ccc1fda088c448fa3d8753bee6ffa87fca41d',
     },
     token: tokens.syrup,
     quoteToken: tokens.wbnb,
